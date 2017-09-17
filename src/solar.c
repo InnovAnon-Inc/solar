8f8acb7d3585fe612c53aa1100fb6d21d912596d
Default User <laurence.a.maddox@gmail.com>
2017-09-17 18:18:42 -0500

    initial commit

--- src/solar.c
+++ src/solar.c
@@ -63,26 +63,25 @@ double AngleOfDay (int day,     /* number of the day */
     /* calculate angle of day */
     DayAngle = (2*MATH_PI*(numOfDays-1)) / AllYearDays;
     return DayAngle;
-
 }
 
 
 /* Function to calculate declination - in radian */
 double Declination (double DayAngle)     /* angle day in radian */
 {
-    double SolarDeclination;
-    /* Solar declination (radian) */
-   #pragma GCC diagnostic push
-   #pragma GCC diagnostic ignored "-Wunsuffixed-float-constants"
-    SolarDeclination = 0.006918
+   double SolarDeclination;
+   /* Solar declination (radian) */
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wunsuffixed-float-constants"
+   SolarDeclination = 0.006918
             - 0.399912 * cos (DayAngle)
             + 0.070257 * sin (DayAngle)
             - 0.006758 * cos (2*DayAngle)
             + 0.000907 * sin (2*DayAngle)
             - 0.002697 * cos (3*DayAngle)
             + 0.00148 * sin (3*DayAngle);
-   #pragma GCC diagnostic pop
-    return SolarDeclination;
+	#pragma GCC diagnostic pop
+   return SolarDeclination;
 }
 
 
@@ -90,20 +89,20 @@ double Declination (double DayAngle)     /* angle day in radian */
 /* Function to calculate Equation of time ( et = TSV - TU ) */
 double EqOfTime (double DayAngle)        /* angle day (radian) */
 {
-    double et;
-    /* Equation of time (radian) */
-   #pragma GCC diagnostic push
-   #pragma GCC diagnostic ignored "-Wunsuffixed-float-constants"
-    et = 0.000075
-         + 0.001868 * cos (DayAngle)
-         - 0.032077 * sin (DayAngle)
-         - 0.014615 * cos (2*DayAngle)
-         - 0.04089 * sin (2*DayAngle);
-    /* Equation of time in hours */
-   #pragma GCC diagnostic pop
-    et = RadToHours(et);
+   double et;
+   /* Equation of time (radian) */
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wunsuffixed-float-constants"
+   et = 0.000075
+        + 0.001868 * cos (DayAngle)
+        - 0.032077 * sin (DayAngle)
+        - 0.014615 * cos (2*DayAngle)
+        - 0.04089 * sin (2*DayAngle);
+   /* Equation of time in hours */
+	#pragma GCC diagnostic pop
+   et = RadToHours(et);
 
-    return et;
+   return et;
 }
 
 /* Calculation of the duration of the day in radian */
@@ -154,13 +153,13 @@ double Excentricity(int day,
     dayAngleRad = AngleOfDay(day, month, year);
 
     /* calculate the excentricity */
-   #pragma GCC diagnostic push
-   #pragma GCC diagnostic ignored "-Wunsuffixed-float-constants"
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wunsuffixed-float-constants"
     E0 = 1.000110 + 0.034221 * cos(dayAngleRad)
             + 0.001280 * sin(dayAngleRad)
             +0.000719 * cos(2*dayAngleRad)
             +0.000077 * sin(2*dayAngleRad);
-   #pragma GCC diagnostic pop
+	#pragma GCC diagnostic pop
 
     return E0;
 }
@@ -370,63 +369,62 @@ double SolarHeight (int tu,     /* universal times (0,1,2,.....,23) */
 //http://www.cs.utsa.edu/~cs1063/projects/Spring2011/Project1/jdn-explanation.html
 */
 int julian(int year, int month, int day) {
-  int a = (14 - month) / 12;
-  int y = year + 4800 - a;
-  int m = month + 12 * a - 3;
-  if (year > 1582 || (year == 1582 && month > 10) || (year == 1582 && month == 10 && day >= 15))
-    return day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
-  else
-    return day + (153 * m + 2) / 5 + 365 * y + y / 4 - 32083;
+   int a = (14 - month) / 12;
+   int y = year + 4800 - a;
+   int m = month + 12 * a - 3;
+   if (year > 1582 || (year == 1582 && month > 10) || (year == 1582 && month == 10 && day >= 15))
+     return day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+   else
+     return day + (153 * m + 2) / 5 + 365 * y + y / 4 - 32083;
 }
 
 
 
-int solar_test(int argc, /*_TCHAR*/ char* argv[])
-{
-   #pragma GCC diagnostic push
-   #pragma GCC diagnostic ignored "-Wunsuffixed-float-constants"
-    int day = 14;
-    int month = 11;
-    int year = 2013;
-    double lat = 39.38;
-    double lon = 22.75;
-    double rlat = 39.38 * pi/180;
-    double rlong = 22.75 * pi/180;
-   #pragma GCC diagnostic pop
+int solar_test(int argc, /*_TCHAR*/ char* argv[]) {
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wunsuffixed-float-constants"
+   int day = 14;
+   int month = 11;
+   int year = 2013;
+   double lat = 39.38;
+   double lon = 22.75;
+   double rlat = 39.38 * pi/180;
+   double rlong = 22.75 * pi/180;
+	#pragma GCC diagnostic pop
 
-    double _AngleOfDay = AngleOfDay ( day , month , year );
-    /*cout << "Angle of day: " << _AngleOfDay << "\n";*/
-    printf ("Angle of day: %g\n", _AngleOfDay);
+   double _AngleOfDay = AngleOfDay ( day , month , year );
+   /*cout << "Angle of day: " << _AngleOfDay << "\n";*/
+   printf ("Angle of day: %g\n", _AngleOfDay);
 
-    double _Declinaison = Declination (_AngleOfDay);
-    /*cout << "Declination (Delta): " << _Declinaison << "\n";*/
-    printf ("Declination (Delta): %g\n", _Declination);
+   double _Declinaison = Declination (_AngleOfDay);
+   /*cout << "Declination (Delta): " << _Declinaison << "\n";*/
+   printf ("Declination (Delta): %g\n", _Declination);
 
-    double _EqOfTime = EqOfTime (_AngleOfDay);
-    /*cout << "Declination (Delta): " << _EqOfTime << "\n";*/
-    printf ("Declination (Delta): %g\n", _EqOfTime);
+   double _EqOfTime = EqOfTime (_AngleOfDay);
+   /*cout << "Declination (Delta): " << _EqOfTime << "\n";*/
+   printf ("Declination (Delta): %g\n", _EqOfTime);
 
-    double _DayDuratInHours = DayDuratInHours (_Declinaison, rlat);
-    /*cout << "Day duration: " << _DayDuratInHours << "\n";*/
-    printf ("Day duration: %g\n", _DayDuratInHours);
+   double _DayDuratInHours = DayDuratInHours (_Declinaison, rlat);
+   /*cout << "Day duration: " << _DayDuratInHours << "\n";*/
+   printf ("Day duration: %g\n", _DayDuratInHours);
 
-    double _Excentricity = Excentricity(day, month, year);
-    /*cout << "Excentricity: " << _Excentricity << "\n";*/
-    printf ("Eccentricity: %g\n", _Excentricity);
+   double _Excentricity = Excentricity(day, month, year);
+   /*cout << "Excentricity: " << _Excentricity << "\n";*/
+   printf ("Eccentricity: %g\n", _Excentricity);
 
-    double _TheoreticRadiation = TheoreticRadiation(day, month, year, rlat);
-    /*cout << "Theoretical radiation: " << _TheoreticRadiation << "\n";*/
-    printf ("Theoretical radiation: %g\n", _TheoreticRadiation);
+   double _TheoreticRadiation = TheoreticRadiation(day, month, year, rlat);
+   /*cout << "Theoretical radiation: " << _TheoreticRadiation << "\n";*/
+   printf ("Theoretical radiation: %g\n", _TheoreticRadiation);
 
-    double _CalclulateSunriseLocalTime = CalclulateSunriseLocalTime
-                    (day, month, year, rlong, rlat);
-    /*cout << "Sunrise Local Time: " << _CalclulateSunriseLocalTime << "\n";*/
-    printf ("Sunrise Local Time: %g\n", _CalclulateSunriseLocalTime);
+   double _CalclulateSunriseLocalTime = CalclulateSunriseLocalTime
+                   (day, month, year, rlong, rlat);
+   /*cout << "Sunrise Local Time: " << _CalclulateSunriseLocalTime << "\n";*/
+   printf ("Sunrise Local Time: %g\n", _CalclulateSunriseLocalTime);
 
-    double _CalculateSunsetLocalTime = CalculateSunsetLocalTime
-        (day, month, year, rlong, rlat);
-    /*cout << "Sunrise Local Time: " << _CalculateSunsetLocalTime << "\n";*/
-    printf ("Sunrise Local Time: %g\n", _CalculateSunsetLocalTime);
+   double _CalculateSunsetLocalTime = CalculateSunsetLocalTime
+       (day, month, year, rlong, rlat);
+   /*cout << "Sunrise Local Time: " << _CalculateSunsetLocalTime << "\n";*/
+   printf ("Sunrise Local Time: %g\n", _CalculateSunsetLocalTime);
 
-    return 0;
+   return 0;
 }
