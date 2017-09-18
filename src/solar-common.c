d3c5ef18676759b58cb7201fe05824da3ede0c2b
Default User <laurence.a.maddox@gmail.com>
2017-09-18 00:38:16 -0500

    looking for a working implementation online (two implementations do not work so far)

--- src/solar-common.c
+++ src/solar-common.c
@@ -16,43 +16,43 @@
 __attribute__ ((const, leaf, nothrow, warn_unused_result))
 double get_PI (void) {
 #ifdef __asm
-   double pi;
-   __asm
-   {
-      fldpi
-      fstp pi
-   }
-   return pi;
+	double pi;
+	__asm
+	{
+		fldpi
+		fstp pi
+	}
+	return pi;
 #else
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wunsuffixed-float-constants"
-   return acos (-1.0);
+	return acos (-1.0);
 	#pragma GCC diagnostic pop
 #endif
 }
 
 __attribute__ ((const, leaf, nothrow, warn_unused_result))
 double degrees2radians (double degrees) {
-    double ret;
+	double ret;
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wunsuffixed-float-constants"
-    degrees = fmod (360 + degrees, 360.0);
+	degrees = fmod (360 + degrees, 360.0);
 	#pragma GCC diagnostic pop
-    ret = degrees * 2 * M_PI / 360;
-    ret = fmod (2 * M_PI + ret, 2 * M_PI);
-    return ret;
+	ret = degrees * 2 * M_PI / 360;
+	ret = fmod (2 * M_PI + ret, 2 * M_PI);
+	return ret;
 }
 
 __attribute__ ((const, leaf, nothrow, warn_unused_result))
 double radians2degrees (double radians) {
-    double ret;
-   radians = fmod (2 * M_PI + radians, 2 * M_PI);
-   ret = radians * 360 / (2 * M_PI);
+	double ret;
+	radians = fmod (2 * M_PI + radians, 2 * M_PI);
+	ret = radians * 360 / (2 * M_PI);
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wunsuffixed-float-constants"
-   ret = fmod (360 + ret, 360.0);
+	ret = fmod (360 + ret, 360.0);
 	#pragma GCC diagnostic pop
-    return ret;
+	return ret;
 }
 
 __attribute__ ((leaf, nonnull (1, 2), nothrow, warn_unused_result))
