677e1434aab7a992710efccf81adc0d888132903
Default User <laurence.a.maddox@gmail.com>
2017-09-26 09:18:31 -0500

    backup

--- autogen.sh
+++ autogen.sh
@@ -1,26 +1,4 @@
 #! /bin/bash
 set -exo nounset
 
-if [ $# -ne 0 ] ; then
-	git pull origin master
-	git submodule foreach git fetch
-	git submodule foreach git checkout master
-	git submodule foreach git merge origin/master
-	if [ -f Makefile ] ; then
-		make distclean
-	fi
-fi
-
-autoreconf --install --verbose --force --include=m4
-
-if [ $# -eq 0 ] ; then exit 0
-else
-	test "x$1" == xmake
-	DIR="$(readlink -f `pwd`)"
-	rm -fr ../solar-build
-	mkdir ../solar-build
-	cd ../solar-build
-	"$DIR/configure" "--prefix=$HOME"
-	make
-	make install
-fi
+autoreconf --verbose --force --install
